e1f96c48c2532150409fc8956294cff5b45f30db
Thomas Dodson <dodson.t@gmail.com>
2014-06-23 11:15:23 -0400

    Added error log with timestamp to sendmail.php

--- includes/sendemail.php
+++ includes/sendemail.php
@@ -28,8 +28,28 @@
 		$mail->AddAddress($to, $to_name);
 		$mail->Subject  = $subject;
 		$mail->Body     = $html_email;
+		$mail->CharSet  = "utf-8";
 
 		$result = $mail->Send();
 		// echo $result ? 'Sent' : 'Error';
+		$timestamp = time();
+		$error = $mail->ErrorInfo; 
+
+		// Logs the time when the send() function was run and whether it was successfull
+		if ($result === false) {
+			$file = './log.txt';
+			// Open the file to get existing content
+			$current = file_get_contents($file);
+			// Append a new person to the file
+			$current .= strftime("%m/%d/%y %I:%M:%S %P", $timestamp) . " $error\n";
+			// Write the contents back to the file
+			file_put_contents($file, $current);
+		}
+		else if ($result === true) {
+			$file = './log.txt';
+			$current = file_get_contents($file);
+			$current .= strftime("%m/%d/%y %I:%M:%S %P", $timestamp) ."card sent\n";
+			file_put_contents($file, $current);
+		}
 	}
 ?>
